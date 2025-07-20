a7b2710d0f2ec87692c5d1e36c7ae50cfefd0ed8
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2025-07-20 20:58:42 +0300

    #add - next config, ts config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: "standalone",
+	images: {
+		formats: ["image/webp"],
+		remotePatterns: [
+			{
+				hostname: "**",
+				pathname: "**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
